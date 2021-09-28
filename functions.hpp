a12472106f62978e9906599901b1724f853f66a3
aurimasruk <aurimas.ruksys@gmail.com>
2021-09-29 00:04:14 +0300

    Add files via upload

---
+++ functions.hpp
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <string>
+
+using namespace std;
+
+void consoleInput();
+
+void fileInput();
+
+void comparison();
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "functions.hpp"
+using namespace std;
+
+/* ---- TO DO ----
+
+
+
+*/
+
+int main(){
+
+    int choise;
+
+    cout << "Choose testing method: " << endl
+    << "---------------------------" << endl
+    << "1. Input in console. " << endl
+    << "2. Input from file. " << endl
+    << "3. Comparison " << endl
+    << " " << endl
+    << "---------------------------" << endl;
+
+
+
+}
